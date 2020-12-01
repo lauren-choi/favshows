2b77aab89ebd6b9487f23583ef2c9bd3cdf9f78e
Lauren Choi <laurenchoi@gmail.com>
2020-12-01 13:07:13 -0500

    split displayList into components

---
+++ src/DisplayItem.js
@@ -0,0 +1,45 @@
+import React from 'react'; 
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
+import ButtonToolbar from 'react-bootstrap/ButtonToolbar';
+import './index.css';
+
+export default class AggregateItem extends React.Component {
+
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+        <Card className="item">
+          <Card.Img variant="top" src={this.props.item.image} style={styles.cardImage} />
+          <Card.Body>
+            <Card.Title>{this.props.item.title}</Card.Title>
+            <Card.Text>
+              Seasons: {this.props.item.seasons}
+            </Card.Text>
+            <Card.Text>
+              Episodes: {this.props.item.episodes}
+            </Card.Text>
+            <Card.Text>
+              Genre: {this.props.item.genre}
+            </Card.Text>
+            <Card.Text>
+              Status: {this.props.item.status}
+            </Card.Text>
+            <Button variant="primary" onClick={() => this.props.addActivity(this.props.item)}>Add to favorites</Button>
+          </Card.Body>
+        </Card>
+      </div>
+    );
+  }
+}
+
+const styles = {
+  cardImage: {
+    objectFit: 'cover',
+    height: '40%'
+  }
+}
--- src/DisplayList.js
+++ src/DisplayList.js
@@ -1,6 +1,7 @@
 import React from 'react'; 
 import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
+import DisplayItem from './DisplayItem';
 import './index.css';
 
 export default class DisplayList extends React.Component {
@@ -17,34 +18,8 @@ export default class DisplayList extends React.Component {
   render() {
     return (
       <div className="filtered-list">
-        {this.props.showsList.map(item => 
-        <Card className="item">
-          <Card.Img variant="top" src={item.image} style={styles.cardImage} />
-          <Card.Body>
-            <Card.Title>{item.title}</Card.Title>
-            <Card.Text>
-              Seasons: {item.seasons}
-            </Card.Text>
-            <Card.Text>
-              Episodes: {item.episodes}
-            </Card.Text>
-            <Card.Text>
-              Genre: {item.genre}
-            </Card.Text>
-            <Card.Text>
-              Status: {item.status}
-            </Card.Text>
-            <Button variant="primary" onClick={() => this.addActivity(item)}>Add to favorites</Button>
-          </Card.Body>
-        </Card>)}
+        {this.props.showsList.map((item) => <DisplayItem item={item} addActivity={this.addActivity} />)}
       </div>
     );
-  }
-}
-
-const styles = {
-  cardImage: {
-    objectFit: 'cover',
-    height: '40%'
   }
 }
